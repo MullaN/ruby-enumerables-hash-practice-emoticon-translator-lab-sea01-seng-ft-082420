ad781ae04878fca39aa3adfb370583f251176937
Andrew Mullan <admullan@gmail.com>
2020-08-04 15:02:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,7 @@ def load_library
   library = YAML.load_file("lib/emoticons.yml")
   binding.pry
   library.collect do |key, value|
+    binding.pry
     library[key] = {english: value[0], japanese: value[1]}
   end
 end
