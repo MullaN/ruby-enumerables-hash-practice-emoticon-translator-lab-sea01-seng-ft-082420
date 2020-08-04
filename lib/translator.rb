90eb2c2274cf33ccef042dd003afbef78ca5a814
Andrew Mullan <admullan@gmail.com>
2020-08-04 15:29:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def get_japanese_emoticon(filepath, emoticon)
   match = library.find do |key, value|
     library[key][:english] == emoticon
   end
-  match ? match[1][:japanese] : "Sorry, could not find emoticon."
+  match ? match[1][:japanese] : "Sorry, that emoticon was not found."
 end
 
 def get_english_meaning
