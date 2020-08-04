ca8aac6baccebe66d559d77f7a72cdfe3152a9e2
Andrew Mullan <admullan@gmail.com>
2020-08-04 15:06:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 def load_library
   yaml_library = YAML.load_file("lib/emoticons.yml")
   binding.pry
-  library yaml_library.collect do |key, value|
+  library = yaml_library.collect do |key, value|
     binding.pry
     yaml_library[key] = {english: value[0], japanese: value[1]}
     binding.pry
