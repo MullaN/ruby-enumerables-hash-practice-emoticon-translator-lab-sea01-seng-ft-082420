5b79719e965a4843361976204169672280cf7dee
Andrew Mullan <admullan@gmail.com>
2020-08-04 14:57:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require 'pry'
 def load_library
   library = YAML.load_file("lib/emoticons.yml")
   library.map do |key, value|
-    key: {english: value[0], japanese: value[1]}
+    key => {english: value[0], japanese: value[1]}
   end
 end
 
